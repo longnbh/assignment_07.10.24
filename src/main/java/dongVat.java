2d244534a270a393f59aba7c48563cf79bd96cfd
longnbh <hoanglongjimmy2005@gmail.com>
2024-10-07 19:12:29 +0700

    add abstract class

--- src/main/java/dongVat.java
+++ src/main/java/dongVat.java
@@ -1,5 +1,9 @@
-public class dongVat {
+public abstract class dongVat {
     private String moveOption; //could be "swim" or "walk"
+
+    public dongVat(String moveOption) {
+        this.moveOption = moveOption;
+    }
 
     public String getMoveOption() {
         return moveOption;
@@ -9,11 +13,29 @@ public class dongVat {
         this.moveOption = moveOption;
     }
 
-    public void printAnimal() {
+    public abstract void move();
+}
 
+public class Pig extends dongVat {
+    public Pig() {
+        super("walk");
     }
+}
 
+public class Duck extends dongVat {
+    public Duck() {
+        super("walk and swim");
+    }
+}
+
+public class Fish extends dongVat {
+    public Fish() {
+        super("swim");
+    }
+}
+
+public class Main {
     public static void main(String[] args) {
-
+        dongVat[] animals =
     }
 }
