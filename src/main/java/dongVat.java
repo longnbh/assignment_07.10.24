17dfa7aed26073ea7c34684b60c525246a145d34
longnbh <hoanglongjimmy2005@gmail.com>
2024-10-07 18:47:38 +0700

    add code file

---
+++ src/main/java/dongVat.java
@@ -0,0 +1,19 @@
+public class dongVat {
+    private String moveOption; //could be "swim" or "walk"
+
+    public String getMoveOption() {
+        return moveOption;
+    }
+
+    public void setMoveOption(String moveOption) {
+        this.moveOption = moveOption;
+    }
+
+    public void printAnimal() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
